c2486d90c88b997bdc9c14130bb6023ddb9e33bd
sayalighanmule <sayalighanmule24960@gmail.com>
2025-08-13 11:31:12 +0530

    add m2 method by dev2

--- Calculator/src/com/app/Test.java
+++ Calculator/src/com/app/Test.java
@@ -8,6 +8,10 @@ public class Test {
 	}
    public void m1()
 	{
-   
+       System.out.pritln("This is m1 method");
    }
+   public void m3()
+	{
+		System.out.println("This is m2 method");
+	}
 }
